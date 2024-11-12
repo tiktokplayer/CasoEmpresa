5b1155b38e4a1fcffe5112b22ed2aa3d8178f486
Bastian Sandoval <baztyansandoval050296@gmail.com>
2024-11-12 16:53:22 -0300

    Act. clases y DDL

--- clases/asignacion.py
+++ clases/asignacion.py
@@ -2,11 +2,11 @@ from clases.departamento import Departamento as departamento
 from clases.empleado import Empleado as empleado
 
 
-class Asignacion:
-    def __init__(self, id_asignacion, id_departamento, id_empleado):
+class Asignacion(departamento,empleado):
+    def __init__(self, id_asignacion, id_departamento , id_empleado):
+        super().__init__(id_departamento)
+        super().__init__(id_empleado)
         self.id_asignacion = id_asignacion
-        self.departamento = departamento(id_departamento)
-        self.empleado = empleado(id_empleado)
     
     #def Validar_asignacion():
     #def Asignacion():
