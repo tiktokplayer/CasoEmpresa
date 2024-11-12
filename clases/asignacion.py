3d67f4948901f6b3c1774026f991a36c020d2df5
nmson25 <85136197+tiktokplayer@users.noreply.github.com>
2024-11-12 16:56:50 -0300

    Update asignacion.py

--- clases/asignacion.py
+++ clases/asignacion.py
@@ -2,12 +2,11 @@ from clases.departamento import Departamento as departamento
 from clases.empleado import Empleado as empleado
 
 
-class Asignacion(departamento, empleado):
-    def __init__(self, id_asignacion, id_departamento, id_empleado):
+class Asignacion(departamento,empleado):
+    def __init__(self, id_asignacion, id_departamento , id_empleado):
         super().__init__(id_departamento)
         super().__init__(id_empleado)
         self.id_asignacion = id_asignacion
-
-
+    
     #def Validar_asignacion():
     #def Asignacion():
